8e0cdc4181b62c5fd24d34661b1f2ca4ee97d6fe
Muhammed Oguz <muhammedaslan1999@outlook.com>
2021-09-16 02:30:15 +0300

    Add brightness functionality

--- js/index.js
+++ js/index.js
@@ -1,9 +1,15 @@
 const hexInput = document.getElementById("input-color");
 const inputColor = document.getElementById("input-box");
+const outputColor = document.getElementById("altered-box");
 const invalidCol = document.getElementById("invalid-color");
 
 const rangeVal = document.getElementById("range-value");
 const rangeInput = document.getElementById("range");
+
+const inputInfo = document.getElementById("input-info");
+const outputInfo = document.getElementById("output-info");
+
+let lighten = true;
 
 hexInput.addEventListener("input", () => {
     const value = checkValidHex(hexInput.value);
@@ -11,7 +17,9 @@ hexInput.addEventListener("input", () => {
         let hex = hexInput.value;
         setColor(inputColor, hex);
 
-        validHex();
+        if (hex[0] === "#") hex = hex.substring(1);
+        if (hex.length === 3) hex = convert6Hex(hex);
+        validHex(hex);
     }
     else {
         clearColor(inputColor);
@@ -21,7 +29,6 @@ hexInput.addEventListener("input", () => {
 });
 
 /**
- * 
  * @param {string} num 
  */
 function checkValidHex(num) {
@@ -34,7 +41,7 @@ function checkValidHex(num) {
 
     // if 3 num hex style, convert 6 style
     if (num.length === 3) {
-        num = num[0] + num[0] + num[1] + num[1] + num[2] + num[2];
+        num = convert6Hex(num);
     }
 
     if (num.length != 6) {
@@ -51,6 +58,29 @@ function updateRangeValue(val) {
     rangeVal.innerText =  val + "%";
 }
 
+function updateOutputColor(val) {
+    let hex = hexInput.value;
+
+    if (hex[0] === "#") hex = hex.substring(1);
+    hex = convert6Hex(hex);
+    
+    // let newHex = val * 1;
+    // newHex = "0000" + newHex;
+    // hex = addHexColor(hex, newHex);
+    
+    let valNum = 0;
+    if (lighten) {
+        valNum = 100 + (12 * (parseInt(val)) );
+    }
+    else {
+        valNum = 100 - val;
+    }
+    outputColor.style.filter = `brightness(${valNum}%)`;
+    outputColor.style.backgroundColor ="#" + hex;
+
+    outputInfo.innerText = `Altered Color: brightness(${valNum}%)`;
+}
+
 // helper function 
 function setColor(element, hex) {
     if (hex[0] !== "#")
@@ -62,9 +92,15 @@ function clearColor(element) {
     element.style.backgroundColor = "";
 }
 
-function validHex() {
+function validHex(hex) {
     rangeInput.disabled = false;
     invalidCol.classList.add("hidden");
+
+    inputInfo.innerText = "Input Color: #" + hex;  
+    
+    outputColor.style.backgroundColor = "#" + hex;
+    outputInfo.innerText = `Altered Color: brightness(${100}%)`;
+
 }
 
 function invalidHex() {
@@ -74,6 +110,24 @@ function invalidHex() {
     updateRangeValue(0);
 
     invalidCol.classList.remove("hidden");
+
+    outputColor.style.backgroundColor = "";
+    outputInfo.innerText = "Altered Color";
+    inputInfo.innerText = "Input Color";
+}
+
+function convert6Hex(num) {
+    if (num.length === 3) {
+        num = num[0] + num[0] + num[1] + num[1] + num[2] + num[2];
+    }
+    return num;
+}
+
+function addHexColor(c1, c2) {
+    var hexStr = (parseInt(c1, 16) + parseInt(c2, 16)).toString(16);
+    while (hexStr.length < 6) { hexStr = '0' + hexStr; } // Zero pad.
+    // if (hexStr.length > 6) return "ffffff";
+    return hexStr;
 }
 
 
